ffdd9988f5b15e07b9858a7b280893f0497e1e79
Paulo <pemsm@cin.ufpe.br>
2022-01-31 21:44:12 -0300

    commit inicial

---
+++ Class2.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class RepositorioSerie : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static RepositorioSerie repositorio = new RepositorioSerie();
+
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSeries();
+                        break;
+                    case "3":
+                        AtualizarSeries();
+                        break;
+                    case "4":
+                        ExcluirSeries();
+                        break;
+                    case "5":
+                        VisualizarSeries();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.ReadLine();
+        }
+
+        private static void InserirSeries()
+        {
+            Console.WriteLine("Inserir nova série");
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=net-5.0&viewFallbackFrom=netcore-5.0
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=net-5.0&viewFallbackFrom=netcore-5.0
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Digite o genêro entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.WriteLine("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Insere(novaSerie);
+        }
+
+        private static void AtualizarSeries()
+        {
+            Console.WriteLine("digite o id da série");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=net-5.0&viewFallbackFrom=netcore-5.0
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=net-5.0&viewFallbackFrom=netcore-5.0
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Digite o genêro entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.WriteLine("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        private static void ExcluirSeries()
+        {
+            Console.WriteLine("digite o id da série");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void VisualizarSeries()
+        {
+            Console.WriteLine("digite o id da série");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada");
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+
+                Console.WriteLine("#ID: {0}: - {1} {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluido*" : ""));
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("MusicFlix ao seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1- Listar Séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
